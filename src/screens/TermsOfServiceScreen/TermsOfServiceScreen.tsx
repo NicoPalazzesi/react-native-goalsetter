2756bc6987173fc8e6ded97a70eb5ea68fd91174
Nicolas Palazzesi <nicolaspalazzesi@gmail.com>
2023-05-14 14:35:01 -0300

    feat: add terms of services text

--- src/screens/TermsOfServiceScreen/TermsOfServiceScreen.tsx
+++ src/screens/TermsOfServiceScreen/TermsOfServiceScreen.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import ScreenContainer from '../../components/ScreenContainer';
 import Header from './components/Header';
 import Colors from '../../constants/Colors';
+import TermsOfServicesText from './components/TermsOfServicesText';
+import { StyleSheet, View } from 'react-native';
 
 const TermsOfServiceScreen = () => (
   <ScreenContainer statusBarColor={Colors.darkPurple}>
     <Header />
+    <View style={styles.ContentContainer}>
+      <TermsOfServicesText />
+    </View>
   </ScreenContainer>
 );
+
+const styles = StyleSheet.create({
+  ContentContainer: {
+    paddingTop: 28,
+    paddingHorizontal: 20,
+  },
+});
 
 export default TermsOfServiceScreen;
---
+++ src/screens/TermsOfServiceScreen/components/TermsOfServicesText.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import Text from '../../../components/Text';
+
+const TermsOfServicesText = () => (
+  <View>
+    <Text
+      text={'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'}
+      style={styles.text}
+    />
+    <Text
+      text={'1.  Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.n. .'}
+      style={styles.text}
+    />
+    <Text
+      text={'2. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.'}
+      style={styles.text}
+    />
+    <Text
+      text={'3. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+    />
+  </View>
+);
+
+const styles = StyleSheet.create({
+  text: {
+    marginBottom: 28,
+  },
+});
+
+export default TermsOfServicesText;
