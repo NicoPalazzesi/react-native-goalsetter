65ab8deb079a50cd7bfc4fb8f27da54ccb3bdab2
Nicolas Palazzesi <nicolaspalazzesi@gmail.com>
2023-05-15 21:32:35 -0300

    feat: add eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react-hooks'],
+  overrides: [
+    {
+      files: ['*.ts', '*.tsx'],
+      rules: {
+        'react-hooks/exhaustive-deps': 'warn',
+      },
+    },
+  ],
 };
